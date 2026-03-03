836512ab7c4341681dd43ad8bd313c41103e295d
Javier ignacio <javielitoignacio22@gmail.com>
2026-03-03 06:59:11 -0400

    otro intento

--- public/script.js
+++ public/script.js
@@ -13,8 +13,35 @@ const productos = [
     nombre: "Jordan 1 Retro",
     precio: 180,
     imagen: "https://static.nike.com/a/images/t_PDP_864_v1/jordan-1-retro-high-og-shoes.png"
+  },
+  {
+    nombre: "Puma Suede Classic",
+    precio: 85,
+    imagen: "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa/global/352634/03/sv01/fnd/PNA/fmt/png"
+  },
+  {
+    nombre: "New Balance 550",
+    precio: 110,
+    imagen: "https://nb.scene7.com/is/image/NB/bb550wt1_nb_02_i?$pdpflexf2$&wid=440&hei=440"
+  },
+  {
+    nombre: "Converse Chuck Taylor All Star",
+    precio: 75,
+    imagen: "https://www.converse.com/on/demandware.static/-/Sites-masterCatalog_Converse/default/dw2c6a9e99/images/hi/101010C_01X1.jpg"
+  },
+  {
+    nombre: "Vans Old Skool",
+    precio: 70,
+    imagen: "https://images.vans.com/is/image/VansEU/VN000D3HY28-HERO?$PDP-FULL-IMAGE$"
+  },
+  {
+    nombre: "Reebok Club C 85",
+    precio: 90,
+    imagen: "https://assets.reebok.com/images/w_600,f_auto,q_auto/club-c-85-shoes.jpg"
   }
 ];
+
+
 
 let carrito = [];
 
